13128c7bee3ce0d515457520e314a379a790fe52
Keith Sistrunk <59321168+KeithSistrunk@users.noreply.github.com>
2026-05-20 18:19:38 +0700

    Update version number to 1.1

--- toggle-cipher-suites.ps1
+++ toggle-cipher-suites.ps1
@@ -8,7 +8,7 @@
     Author        : Keith Sistrunk
     Date Created  : 2025-04-09
     Last Modified : 2024-04-09
-    Version       : 1.0
+    Version       : 1.1
 
 .TESTED ON
     Date(s) Tested  : 2025-04-09
